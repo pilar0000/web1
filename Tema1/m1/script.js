45f024d0031be1574110a9aaed2a6659ffd9b013
pilar0000 <pilarescolarsuarez@hotmail.com>
2026-09-23 21:19:00 +0200

    añadido topo
    aparece el topo aleatoriamente en un hueco, y al pulsarlo desaparece y suma un punto.

--- Tema1/m1/script.js
+++ Tema1/m1/script.js
@@ -1,1 +1,28 @@
 
+let puntuacion = 0;
+const marcador = document.getElementById("puntos");
+
+const topos = document.querySelectorAll(".topo");
+
+function mostrarTopo() {
+
+    topos.forEach(topo => {
+        topo.classList.remove("activo");
+    });
+
+    const numero = Math.floor(Math.random() * topos.length);
+
+    topos[numero].classList.add("activo");
+}
+
+setInterval(mostrarTopo, 1000);
+
+topos.forEach(topo => {
+    topo.addEventListener("click", () => {
+        if (topo.classList.contains("activo")) {
+            puntuacion++;
+            marcador.textContent = puntuacion;
+            topo.classList.remove("activo");
+        }
+    });
+});
